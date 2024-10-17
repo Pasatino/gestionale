068a8c38acd32e30af94f29bef276877b71a05e6
Pasatino <andreipasatino@gmail.com>
2024-10-17 19:45:03 +0200

    Sistemato problema con boostrap, bastava cambiare class in className

--- frontend/src/app/prodotti/page.tsx
+++ frontend/src/app/prodotti/page.tsx
@@ -1,41 +1,37 @@
-import React from 'react';
+import React from "react";
 
-const ProdottiPage = () =>{
-    return (
-        <div>
-            <table class="table">
-  <thead class="primary">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-        </div>
-    
-        
-    )
-}
+const ProdottiPage = () => {
+  return (
+    <table className="table table-primary">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
-export default ProdottiPage
+export default ProdottiPage;
